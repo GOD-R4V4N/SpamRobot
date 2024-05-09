4bd370e7498ca084852518fbfc2f3cd9d1e5d8e8
GOD RAVAN <167273277+GOD-R4V4N@users.noreply.github.com>
2024-05-09 15:41:15 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,3 @@
-
-
 import logging
 
 from telethon import TelegramClient
@@ -12,8 +10,8 @@ logging.basicConfig(format='[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s'
 
 
 # VALUES REQUIRED FOR XBOTS
-API_ID = 18136872
-API_HASH = "312d861b78efcd1b02183b2ab52a83a4"
+API_ID = 20851717
+API_HASH = "0217fe5cd94ff3f4d4555b1551670144"
 CMD_HNDLR = getenv("CMD_HNDLR", default=".")
 HEROKU_APP_NAME = getenv("HEROKU_APP_NAME", None)
 HEROKU_API_KEY = getenv("HEROKU_API_KEY", None)
@@ -21,10 +19,10 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY", None)
 BOT_TOKEN = getenv("BOT_TOKEN", default=None)
 
 
-SUDO_USERS = list(map(lambda x: int(x), getenv("SUDO_USERS", default="6922271843").split()))
+SUDO_USERS = list(map(lambda x: int(x), getenv("SUDO_USERS", default="6927241780").split()))
 for x in MAHAK:
     SUDO_USERS.append(x)
-OWNER_ID = int(getenv("OWNER_ID", default="6922271843"))
+OWNER_ID = int(getenv("OWNER_ID", default="6927241780"))
 SUDO_USERS.append(OWNER_ID)
 
 
