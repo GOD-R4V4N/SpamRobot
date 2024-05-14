35ca7c1c229507ba1cc330976dde036145be5a56
GOD RAVAN <167273277+GOD-R4V4N@users.noreply.github.com>
2024-05-14 13:58:57 +0530

    Update raid.py

--- Asuraa/modules/raid.py
+++ Asuraa/modules/raid.py
@@ -184,7 +184,7 @@ async def craid(e):
 
         try:
             if uid in MAHAK:
-                await e.reply("✦ ɴᴏ, ᴛʜɪꜱ ɢᴜʏ ɪꜱ xsᴘᴀᴍ'ꜱ ᴏᴡɴᴇʀ.")
+                await e.reply("✦ ɴᴏ, ᴛʜɪꜱ ɢᴜʏ ɪꜱ sᴘᴀᴍ'ꜱ ᴏᴡɴᴇʀ.")
             elif uid == OWNER_ID:
                 await e.reply("✦ ɴᴏ, ᴛʜɪꜱ ɢᴜʏ ɪꜱ ᴏᴡɴᴇʀ ᴏꜰ ᴛʜᴇꜱᴇ ʙᴏᴛꜱ.")
             elif uid in SUDO_USERS:
